29ed64c6613cc982b50ff95654323b86919b795a
elvynia <elvynia@gmail.com>
2017-05-19 09:51:47 +0200

    Amélioration classe Main.

--- src/fr/formation/gamebook/Main.java
+++ src/fr/formation/gamebook/Main.java
@@ -2,11 +2,11 @@ package fr.formation.gamebook;
 
 public class Main {
 
-	public static void main(String[] args) throws Exception {
-		if (args.length > 0) {
+	public static void main(String[] args) {
+		if (args.length > 0 ) {
 			new GameBook(args[0]).run();
 		} else {
-			System.out.println("Usage : Main path_to_xml_book");
+			System.err.println("Usage : Main <path_to_xml_data>");
 		}
 	}
 }
